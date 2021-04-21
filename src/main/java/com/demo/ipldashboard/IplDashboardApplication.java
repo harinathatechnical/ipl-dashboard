5e6db8b9e3e4da7668e25f2ab12d8c714d98ec6d
harinathaK <harinatha.k@gmail.com>
2021-04-21 09:28:01 +0530

    Initial commet with jpa changes

---
+++ src/main/java/com/demo/ipldashboard/IplDashboardApplication.java
@@ -0,0 +1,14 @@
+package com.demo.ipldashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.ArrayList;
+
+@SpringBootApplication
+public class IplDashboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IplDashboardApplication.class, args);
+	}
+}
